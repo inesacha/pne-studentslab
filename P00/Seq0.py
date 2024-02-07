cd20417bd6d03c60eb01d7dc831be5a1bbc4e120
inesacha <ij.achard.2023@alumnos.urjc.es>
2024-02-07 18:33:18 +0100

    This is going to be my first but not last commit

--- P00/Seq0.py
+++ P00/Seq0.py
@@ -14,8 +14,39 @@ def seq_read_fasta(filename):
     print("The first 20 bases are:")
     print(list_contents[:20])
 
-def seq_len():
-    gene_list = ["U5", "ADA", "FRAT1", "FXN"]
-    for i in gene_list:
-        i += ".fa"
+def seq_len(seq):
+    FOLDER = "../sequences/"
+    FILENAME = seq + ".fa"
+    file_contents = Path(FOLDER + FILENAME).read_text()
+    header = file_contents.find("\n")
+
+    body = file_contents[header:]
+
+    list_contents = body.replace("\n", "")
+    print("Gene", seq + "-> Length:", len(list_contents))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
