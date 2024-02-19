5f1631347dde9fd4e3f4ef51fa04e5282d1f86f8
inesacha <ij.achard.2023@alumnos.urjc.es>
2024-02-19 15:03:00 +0100

    This is going to be my first but not last commit

--- P00/Seq0.py
+++ P00/Seq0.py
@@ -19,18 +19,43 @@ def seq_len(seq):
     FILENAME = seq + ".fa"
     file_contents = Path(FOLDER + FILENAME).read_text()
     header = file_contents.find("\n")
-
     body = file_contents[header:]
 
     list_contents = body.replace("\n", "")
     print("Gene", seq + "-> Length:", len(list_contents))
 
-def seq_count_base(seq, base):
-    for g in genes:
-        for b in bases:
-            filename = seq + g + ".txt"
-            bases = seq.read(f)
-            total = seq.read(bases, b)
+def seq_count_base(bases):
+    #for g in genes:
+     #   for b in bases:
+     #       filename = seq + g + ".txt"
+      #      bases = seq.read(filename)
+       #     total = seq.read(bases, b)
+
+    for g in bases:
+        FOLDER = "../sequences/"
+        FILENAME = g + ".fa"
+        file_contents = Path(FOLDER + FILENAME).read_text()
+        header = file_contents.find("\n")
+        index0.0 = file_contents[header:]
+        c_c = 0
+        c_g = 0
+        c_a = 0
+        c_t = 0
+        for b in index:
+            if b == "A":
+                c_a += 1
+            elif b == "G":
+                c_g += 1
+            elif b == "T":
+                c_t += 1
+            elif b == "C":
+                c_c += 1
+        print(g, c_c)
+        return
+
+bases = ["ADA"]
+lol = seq_count_base(bases)
+
 
 
 
--- P00/e4.py
+++ P00/e4.py
@@ -1,1 +1,3 @@
+from Seq0 import *
+
 bases = ["A", "T", "C", "G"]
