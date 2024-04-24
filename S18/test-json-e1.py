bc4c9afb27923d8463afba2f8d04d61d610fba89
inesacha <ij.achard.2023@alumnos.urjc.es>
2024-04-24 12:12:48 +0200

    Commit!

--- S18/test-json-e1.py
+++ S18/test-json-e1.py
@@ -3,34 +3,33 @@ import termcolor
 from pathlib import Path
 
 # -- Read the json file
-jsonstring = Path("people-3.json").read_text()
+jsonstring = Path("people-e1.json").read_text()
 
 # Create the object person from the json string
 person = json.loads(jsonstring)
 
-# Person is now a dictionary. We can read the values
-# associated to the fields 'Firstname', 'Lastname' and 'age'
+# Get the people's list
+peopleNumbers = person['People']
+
+# Print the number of people in the list
+termcolor.cprint("Total people in the database: ", 'green', end='')
+print(len(peopleNumbers))
 
 # Print the information on the console, in colors
 print()
-termcolor.cprint("Name: ", 'green', end="")
-print(person['Firstname'], person['Lastname'])
-termcolor.cprint("Age: ", 'green', end="")
-print(person['age'])
+for i, dictnum in enumerate(person['People']):
+    termcolor.cprint("Name: ", 'green', end="")
+    print(person['People'][i]['Firstname'], person['People'][i]['Lastname'])
+    termcolor.cprint("Age: ", 'green', end="")
+    print(person['People'][i]['age'])
+    phoneNumbers = person['People'][i]['phoneNumber']
+    termcolor.cprint("Phone numbers: ", 'green', end='')
+    print(len(phoneNumbers))
+    for i, dictnum in enumerate(phoneNumbers):
+        termcolor.cprint("  Phone " + str(i) + ": ", 'blue')
+        # The element num contains 2 fields: number and type
+        termcolor.cprint("\t Type: ", 'red', end='')
+        print(dictnum['type'])
+        termcolor.cprint("\t Number: ", 'red', end='')
+        print(dictnum['number'])
 
-# Get the phoneNumber list
-phoneNumbers = person['phoneNumber']
-
-# Print the number of elements in the list
-termcolor.cprint("Phone numbers: ", 'green', end='')
-print(len(phoneNumbers))
-
-# Print all the numbers
-for i, dictnum in enumerate(phoneNumbers):
-    termcolor.cprint("  Phone " + str(i + 1) + ": ", 'blue')
-
-    # The element num contains 2 fields: number and type
-    termcolor.cprint("\t- Type: ", 'red', end='')
-    print(dictnum['type'])
-    termcolor.cprint("\t- Number: ", 'red', end='')
-    print(dictnum['number'])
